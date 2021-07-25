2190dfc27da86cf302b0b231fc3b674b93f64b2e
docustack <87936484+docustack@users.noreply.github.com>
2021-07-25 19:54:00 +0200

    Update conf.py

--- user_windows/source/conf.py
+++ user_windows/source/conf.py
@@ -50,10 +50,10 @@ master_doc = 'contents'
 
 # General information about the project.
 project = 'Windows Client Guide'
-copyright = '2018, GreenCity Solutions'
-author = 'GreenCity Solutions'
+copyright = '2021, Docustack (Pty) Ltd'
+author = 'Docustack'
 
-prodname = 'DIVVIED'
+prodname = 'Docustack'
 
 rst_epilog = '.. |prodname| replace:: %s' % prodname
 
